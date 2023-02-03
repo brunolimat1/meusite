331dc68b8b0be73959523ca15de254492bb054fc
Hacker from the Brazooka <104094262+brunolimat1@users.noreply.github.com>
2023-02-03 16:53:15 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+		
+    <head>
+			<body style="background-color:Khaki;">
+      <title>Teste da Linguagem PQP</title>
+			<meta charset="utf-8">
+	
+    </head>
+    
+    
+    </html>
